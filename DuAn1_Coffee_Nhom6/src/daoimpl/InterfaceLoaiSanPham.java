28c56e4e824c5bb60e7841d0304d385ff224c6b4
PhamAnhTuan121 <phamtuan785aa@gmail.com>
2024-04-03 16:54:02 +0700

    Thêm sản phẩm DAO

--- DuAn1_Coffee_Nhom6/src/daoimpl/InterfaceLoaiSanPham.java
+++ DuAn1_Coffee_Nhom6/src/daoimpl/InterfaceLoaiSanPham.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package daoimpl;
-import Model.*;
+import model.*;
 import java.util.List;
 /**
  *
